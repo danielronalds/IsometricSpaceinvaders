c9b007023544d09c024afb1ee5f167d6fd517e16
danielronalds <67301522+danielronalds@users.noreply.github.com>
2021-07-30 14:55:16 +1200

    Imported my game engine

--- IsometricSpaceinvaders/Form1.Designer.cs
+++ IsometricSpaceinvaders/Form1.Designer.cs
@@ -29,13 +29,33 @@ namespace IsometricSpaceinvaders
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(684, 661);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- IsometricSpaceinvaders/Form1.cs
+++ IsometricSpaceinvaders/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IsometricGameEngine;
 
 namespace IsometricSpaceinvaders
 {
     public partial class Form1 : Form
     {
+        private IsometricGrid3D isometricGrid = new IsometricGrid3D(342, 300, 10, 2);
+
+        private IsometricGrid2D gameGrid;
+
+        private Renderer2D test;
+
+        private RenderComponent testTwo;
+
+        Graphics g;
+
         public Form1()
         {
             InitializeComponent();
+
+            test = new Renderer2D(isometricGrid.to2D(0), TileMapTemplates.FilledGrid(isometricGrid), Properties.Resources._64x64_isometric_tile);
+
+            gameGrid = isometricGrid.to2D(1);
+
+            testTwo = new RenderComponent(Properties.Resources.space_invader, gameGrid.getPoint(5, 5));
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+
+            test.Render(g);
+
+            testTwo.Render(g);
         }
     }
 }
